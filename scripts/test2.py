0b44603eadaae5eae244338209f8256d856a4c3f
Jim Hall <jimhallsun@gmail.com>
2020-04-23 13:20:28 -0400

    trying new script for gh actions

--- scripts/test2.py
+++ scripts/test2.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python
 
 import glob
+import os
+import sys
 print(glob.glob('*'))
+
+opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
+
+if __name__ == "__main__":
+    if "-c" in opts: # categories
+        print("Adding new categories directories if necessary")
+        typedir = 'categories'
+    elif "-t" in opts: # tags
+        print("Adding new tags directories if necessary")
+        typedir = 'tags'
+    else:
+        raise SystemExit(f"Usage: {sys.argv[0]} (-c | -t ) <arguments>...")
