2cd1d0bbe9c1202d2c912bcd78b2b7641a32a31d
Jim Hall <jimhallsun@gmail.com>
2020-04-28 16:18:24 -0400

    categories add parens to dedicated; removed sleep from tests.py and put in the GitHub action

--- scripts/test.py
+++ scripts/test.py
@@ -4,7 +4,6 @@ from datetime import datetime
 import os
 import glob
 import sys
-import time
 
 # Source: https://stackoverflow.com/questions/11061058/using-htmlparser-in-python-3-2
 class MLStripper(HTMLParser):
@@ -41,7 +40,6 @@ def missing_dirs(typedir):
     create_dirs = catset.difference(existing_dirs)
     return create_dirs
 
-time.sleep(3)
 #https://realpython.com/python-command-line-arguments/
 opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
 baseurl = 'https://jimhall.github.io/TestBlog4/'
@@ -64,52 +62,35 @@ if __name__ == "__main__":
         typedir = 'categories'
         print("Adding new categories directories if necessary")
         print('RUN = ' + dt_string + ' ' + typedir + '\n')
-#        with open('catreport.txt', 'wt') as cout:
-#            cout.write('RUN = ' + dt_string + '\n')
     elif "-t" in opts: # tags
         typedir = 'tags'
         print("Adding new tags directories if necessary")
         print('RUN = ' + dt_string + ' ' +  typedir + '\n')
-#        with open('tagreport.txt', 'wt') as tout:
-#            tout.write('RUN = ' + dt_string + '\n')
     else:
         raise SystemExit(f"Usage: {sys.argv[0]} (-c | -t ) <arguments>...")
-
 
 # Simple logic if there are no new dirs to create
 # https://stackoverflow.com/questions/29064227/how-can-i-test-to-see-if-a-defaultdictset-is-empty-in-python
     create_dirs = missing_dirs(typedir)
     if not create_dirs:
-#        if typedir == 'categories':
-#            with open('catreport.txt', 'a') as cout:
-#                cout.write(typedir + ': NONE' + '\n')
-#        if typedir == 'tags':
-#            with open('tagreport.txt', 'a') as tout:
-#                tout.write(typedir + ': NONE' + '\n')
         # Preferred way to exit:
         # https://stackoverflow.com/questions/73663/how-to-terminate-a-python-script
-        print('No new ' + typedir + ' directories to create\n')
+        print('No new ' + typedir + ' directories to create')
         sys.exit()
     else:
-        print('TYPEDIR\tNAME\n')
+        print('TYPEDIR\tNAME')
         with open(tmpfile, 'wt') as sout:
             sout.write('1\n')
 
     for dir in create_dirs:
         basedir = typedir + '/' + dir
         indexmd = basedir + '/index.md'
-#        print(basedir)
-        print(typedir + '\t' + dir + '\n')
-#        print(indexmd)
+        print(typedir + '\t' + dir)
         os.mkdir(basedir)
         with open(indexmd, 'wt') as fout:
             if typedir == 'categories':
                 fout.write(catindexmd)
-#                with open('catreport.txt', 'a') as cout:
-#                    cout.write(typedir + 'file: ' + indexmd + '\n')
             if typedir == 'tags':
                 fout.write(tagindexmd)
-#                with open('tagreport.txt', 'a') as tout:
-#                    tout.write(typedir + 'file: ' + indexmd + '\n')
             fout.write('title: ' + dir + '\n')
             fout.write('---\n')
